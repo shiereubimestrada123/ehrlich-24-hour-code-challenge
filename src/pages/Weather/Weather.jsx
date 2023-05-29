04a7501eb45ee9e86a1b3d6a1ab5f4fbead56f0d
bim <shiereubim.estrada@gmail.com>
2023-05-29 22:19:27 +0800

    Add hook for window resize and render weather data for mobile size

--- src/pages/Weather/Weather.jsx
+++ src/pages/Weather/Weather.jsx
@@ -2,34 +2,61 @@ import { Link } from "react-router-dom";
 import moment from 'moment';
 
 import Main from '../../components/Main'
+import useWindowSize from '../../hooks/useWindowSize.jsx'
 
 function Weather({ data }) {
-	const tableHeader = ['Date (mm/dd/yyyy)', 'Temp(F)', 'Description', 'Main', 'Pressure', 'Humidity']
+	const size = useWindowSize();
+
+	const desktopHeader = ['Date (mm/dd/yyyy)', 'Temp(F)', 'Description', 'Main', 'Pressure', 'Humidity']
+	const mobileHeader = ['Date (mm/dd/yyyy)', 'Temperature (F)']
 
   return (
     <section>
-      <Main>
-				<table className="table-auto border-collapse border border-slate-400">
-					<thead>
-						<tr>
-							{tableHeader.map((h, i) => (
-								<th key={i} className='border border-slate-300'>{h}</th>
-							))}
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td className="border border-slate-300">{moment().format('MM/DD/YYYY')}</td>
-							<td className="border border-slate-300">{data?.main?.temp}</td>
-							<td className="border border-slate-300">{data.weather ? data?.weather[0]?.description : null}</td>
-							<td className="border border-slate-300">{data.weather ? data?.weather[0]?.main : null}</td>
-							<td className="border border-slate-300">{data?.main?.pressure}</td>
-							<td className="border border-slate-300">{data?.main?.humidity}</td>
-						</tr>
-					</tbody>
-				</table>
-				<Link to='/home' className="w-fit bg-slate-400 self-end py-2 px-4">Back</Link>
-			</Main>
+			<Main>
+				{size.width > 600 ? (
+					<>
+						<table className="table-auto border-collapse border border-slate-400">
+							<thead>
+								<tr>
+									{desktopHeader.map((h, i) => (
+										<th key={i} className='border border-slate-300'>{h}</th>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td className="border border-slate-300">{moment().format('MM/DD/YYYY')}</td>
+									<td className="border border-slate-300">{data?.main?.temp}</td>
+									<td className="border border-slate-300">{data.weather ? data?.weather[0]?.description : null}</td>
+									<td className="border border-slate-300">{data.weather ? data?.weather[0]?.main : null}</td>
+									<td className="border border-slate-300">{data?.main?.pressure}</td>
+									<td className="border border-slate-300">{data?.main?.humidity}</td>
+								</tr>
+							</tbody>
+						</table>
+						<Link to='/home' className="w-fit bg-slate-400 self-end py-2 px-4">Back</Link>
+					</>
+				) : (
+					<>
+						<table className="table-auto border-collapse border border-slate-400">
+							<thead>
+								<tr>
+									{mobileHeader.map((h, i) => (
+										<th key={i} className='border border-slate-300'>{h}</th>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td className="border border-slate-300">{moment().format('MM/DD/YYYY')}</td>
+									<td className="border border-slate-300">{data?.main?.temp}</td>
+								</tr>
+							</tbody>
+						</table>
+						<Link to='/home' className="w-fit bg-slate-400 self-end py-2 px-4">Back</Link>
+					</>
+				)}
+      </Main>
     </section>
   )
 }
